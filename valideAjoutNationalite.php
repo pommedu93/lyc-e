d80c7b5bcae470c678f43da554aafdf554989206
pommedu93 <pommela23@gmail.com>
2026-02-12 12:12:01 +0100

    on continue comme ça

---
+++ valideAjoutNationalite.php
@@ -0,0 +1,30 @@
+<?php 
+include "header.php";
+include "connexionPDO.php";
+$libelle=$_POST['libelle'];//donné du libellé du form
+
+$req=$monPdo->prepare("insert into nationalite(libelle) values(:libelle)");
+$req->bindParam(':libelle', $libelle);
+$req->execute();
+echo'<div class="container mt-5">'
+?>
+if($nb ==1){
+    echo'<div class="alert alert-success" role="alert">
+ La nationalité est ajoutée
+</div>';
+}else{
+    echo'<div class="alert alert-danger" role="alert">
+ La nationalité est rejetter
+</div>';
+
+}
+
+
+
+</div>
+
+
+<?php include "footer.php";
+
+?>
+22m30
